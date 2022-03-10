b3af8afa6e4f5a7cbb55284ac9249ef68769baac
Ikka1 <95162383+Ikka1@users.noreply.github.com>
2022-03-10 21:25:32 +0800

    CSS-Update the forecast font

--- src/index.js
+++ src/index.js
@@ -28,7 +28,15 @@ function formatDate(timezone) {
 function formatDay(timestamp) {
   let date = new Date(timestamp * 1000);
   let day = date.getDay();
-  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
   return days[day];
 }
 
